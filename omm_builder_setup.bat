34f9c14689fb3c78e58696cc2e52d420263c6ffe
U-DESKTOP-F0TJMO2\Utilisateur <peachy.peach.sm64@gmail.com>
2022-09-25 16:00:16 +0200

    new setup

--- omm_builder_setup.bat
+++ omm_builder_setup.bat
@@ -1,23 +1,10 @@
 @echo off
-setlocal enableDelayedExpansion
-set root_path=C:
-if exist "%root_path%/msys64/mingw64.exe" (
-	set mingw64_path=%root_path%/msys64/mingw64.exe
-) else (
-	set /p mingw64_path=< mingw64_path
-	if "!mingw64_path!" == "" (
-		cls
-		echo Trying to locate mingw64.exe in %root_path%...
-		where /R %root_path% mingw64.exe > mingw64_path
-		set /p mingw64_path=< mingw64_path
-		if "!mingw64_path!" == "" (
-			cls
-			echo Couldn't find mingw64.exe, maybe it's not located in %root_path%?
-			echo To change the search path, open omm_builder.bat in a text editor and change the value of root_path.
-			pause
-			exit
-		)
-	)
+set mingw64exe="%appdata%\msys2\mingw64.exe"
+if not exist %mingw64exe% (
+	echo mingw64.exe not found. Installing latest version...
+	curl -Lk -o msys2.exe --url "https://github.com/msys2/msys2-installer/releases/download/2022-09-04/msys2-x86_64-20220904.exe"
+	msys2.exe install --root "%appdata%\msys2" --confirm-command
+	del msys2.exe
 )
 if exist "sm64ex-omm-test" (
 	set omm_repo=https://github.com/PeachyPeachSM64/sm64ex-omm-test.git
@@ -27,8 +14,13 @@ if exist "sm64ex-omm-test" (
 echo echo "Setting up OMM Builder and its dependencies..." > omm_builder_setup.sh
 echo pacman -Syu --noconfirm >> omm_builder_setup.sh
 echo pacman -Syu --noconfirm zip unzip p7zip make git mingw-w64-i686-gcc mingw-w64-x86_64-gcc mingw-w64-i686-glew mingw-w64-x86_64-glew mingw-w64-i686-SDL2 mingw-w64-i686-SDL mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL python3 >> omm_builder_setup.sh
-echo git config --global credential.helper store >> omm_builder_setup.sh
-echo git clone --single-branch %omm_repo% -b master sm64ex-omm >> omm_builder_setup.sh
-echo mv -f mingw64_path sm64ex-omm/mingw64_path >> omm_builder_setup.sh
+if not exist "omm_builder.sh" (
+	echo git config --global credential.helper store >> omm_builder_setup.sh
+	echo git clone --single-branch %omm_repo% -b master sm64ex-omm >> omm_builder_setup.sh
+	echo cd sm64ex-omm >> omm_builder_setup.sh
+)
+echo git reset --hard HEAD >> omm_builder_setup.sh
+echo git clean -f -d >> omm_builder_setup.sh
+echo git pull >> omm_builder_setup.sh
 echo rm -f omm_builder_setup.sh >> omm_builder_setup.sh
-start !mingw64_path! bash omm_builder_setup.sh
+%mingw64exe% bash omm_builder_setup.sh
